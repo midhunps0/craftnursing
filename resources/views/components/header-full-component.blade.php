0aaf25b015a3c34ee74a8e86fc72031b5b11ce95
Reshma K R <reshmakanjoorparambil@gmail.com>
2024-03-07 09:01:16 +0530

    commit after commit

--- resources/views/components/header-full-component.blade.php
+++ resources/views/components/header-full-component.blade.php
@@ -21,7 +21,7 @@
                                 <a class="cursor-pointer {{ Request::is('contact') ? 'text-teal font-bold underline' : '' }}" @click="$dispatch('linkaction', {link: '{{route('contact')}}', route: 'contact'}); console.log('event dispatched');">Contact</a> 
                             </div>
                             <div x-data="{ open : false }" class="pt-3 items-center text-right w-2/3 md:hidden items-center ">
-                                <button @click="open = !open" class="">
+                                <button @click="open = !open" title="menu button">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mt-4 mr-3 " viewBox="0 0 448 512">
                                     <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                                     <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
