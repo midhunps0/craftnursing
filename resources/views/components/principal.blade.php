515d37f38457f50aa0a3a4ce8aaa235abadc6863
midhunps <midhunps.0@gmail.com>
2024-08-22 14:05:03 +0530

    modified prici message layout

--- resources/views/components/principal.blade.php
+++ resources/views/components/principal.blade.php
@@ -1,13 +1,17 @@
 <div>
     <div class=" shadow p-2 my-4">
         <h class="text-teal text-base md:text-xl ml-4 lg:ml-0 font-light">Principal's Message</h>
-        <img src="{{url('/images/principal_img.webp')}}" class="w-96 my-2" alt="" >
-        {{-- <p class="text-justify font-extralight  mb-2 text-sm md:text-base 2xl:text-base xl:text-base  mr-6 ml-6 font-inter text-black lg:text-base  lg:text-justify lg:font-extralight lg:ml-0   md:mt-6 lg:mt-0"> --}}
-        <div class="text-justify font-extralight  mb-2 text-sm md:text-base 2xl:text-base xl:text-base  mr-6 ml-6 font-inter text-black lg:text-base  lg:text-justify lg:font-extralight">
-            <p>“Every nurse was drawn to Nursing, because of a desire to care, to serve, or to help.
-            I am very proud to be the principal of this wonderful school. We welcome all aspiring young minds and caring hearts to join the Noble profession of Nursing and pursue our knowledge and skills to serve the world at large.</p>
-            <p>Our CRAFT SCHOOL OF NURSING, offers 3-year Diploma programme in General Nursing and Midwifery (GNM) recognized by KNC and INC. The focus of our institute is to develop the personality of each student and to prepare the nurses with right knowledge, kind and proper attitude. Along with our faculty and supportive staffs, we strive to provide quality nursing education to render standard nursing care to health sector.</p>
-            <p>I, as the Principal of “CRAFT SCHOOL OF NURSING” look forward to see this institute as one of the best Nursing Institution.”</p>
+        <div class="flex flex-row flex-wrap">
+            <div class="w-1/3">
+                <img src="{{url('/images/principal_img.webp')}}" class="w-96 my-2" alt="" >
             </div>
+            {{-- <p class="text-justify font-extralight  mb-2 text-sm md:text-base 2xl:text-base xl:text-base  mr-6 ml-6 font-inter text-black lg:text-base  lg:text-justify lg:font-extralight lg:ml-0   md:mt-6 lg:mt-0"> --}}
+            <div class="w-3/4 text-justify font-extralight  mb-2 text-sm md:text-base 2xl:text-base xl:text-base  mr-6 ml-6 font-inter text-black lg:text-base  lg:text-justify lg:font-extralight">
+                <p>“Every nurse was drawn to Nursing, because of a desire to care, to serve, or to help.
+                I am very proud to be the principal of this wonderful school. We welcome all aspiring young minds and caring hearts to join the Noble profession of Nursing and pursue our knowledge and skills to serve the world at large.</p>
+                <p>Our CRAFT SCHOOL OF NURSING, offers 3-year Diploma programme in General Nursing and Midwifery (GNM) recognized by KNC and INC. The focus of our institute is to develop the personality of each student and to prepare the nurses with right knowledge, kind and proper attitude. Along with our faculty and supportive staffs, we strive to provide quality nursing education to render standard nursing care to health sector.</p>
+                <p>I, as the Principal of “CRAFT SCHOOL OF NURSING” look forward to see this institute as one of the best Nursing Institution.”</p>
+            </div>
+        </div>
     </div>
 </div>
