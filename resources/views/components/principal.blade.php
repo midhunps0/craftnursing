1553d2e44cca4ecefae2afce483f5fec2affe3bb
midhunps <midhunps.0@gmail.com>
2024-08-22 14:08:48 +0530

    modified prici message layout

--- resources/views/components/principal.blade.php
+++ resources/views/components/principal.blade.php
@@ -2,11 +2,11 @@
     <div class=" shadow p-2 my-4">
         <h class="text-teal text-base md:text-xl ml-4 lg:ml-0 font-light">Principal's Message</h>
         <div class="flex flex-row flex-wrap gap-2">
-            <div class="w-1/4">
+            <div class="w-1/3 pr-3">
                 <img src="{{url('/images/principal_img.webp')}}" class="w-96 my-2" alt="" >
             </div>
             {{-- <p class="text-justify font-extralight  mb-2 text-sm md:text-base 2xl:text-base xl:text-base  mr-6 ml-6 font-inter text-black lg:text-base  lg:text-justify lg:font-extralight lg:ml-0   md:mt-6 lg:mt-0"> --}}
-            <div class="w-3/4 text-justify font-extralight  mb-2 text-sm md:text-base 2xl:text-base xl:text-base  mr-6 ml-6 font-inter text-black lg:text-base  lg:text-justify lg:font-extralight">
+            <div class="w-2/3 text-justify font-extralight  mb-2 text-sm md:text-base 2xl:text-base xl:text-base  pl-3 font-inter text-black lg:text-base  lg:text-justify lg:font-extralight">
                 <p>“Every nurse was drawn to Nursing, because of a desire to care, to serve, or to help.
                 I am very proud to be the principal of this wonderful school. We welcome all aspiring young minds and caring hearts to join the Noble profession of Nursing and pursue our knowledge and skills to serve the world at large.</p>
                 <p>Our CRAFT SCHOOL OF NURSING, offers 3-year Diploma programme in General Nursing and Midwifery (GNM) recognized by KNC and INC. The focus of our institute is to develop the personality of each student and to prepare the nurses with right knowledge, kind and proper attitude. Along with our faculty and supportive staffs, we strive to provide quality nursing education to render standard nursing care to health sector.</p>
