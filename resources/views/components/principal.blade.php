3266a822a82bbf8e2dc0e71c2ca3fbaf38bdc18d
midhunps <midhunps.0@gmail.com>
2024-08-22 14:06:07 +0530

    modified prici message layout

--- resources/views/components/principal.blade.php
+++ resources/views/components/principal.blade.php
@@ -1,8 +1,8 @@
 <div>
     <div class=" shadow p-2 my-4">
         <h class="text-teal text-base md:text-xl ml-4 lg:ml-0 font-light">Principal's Message</h>
-        <div class="flex flex-row flex-wrap">
-            <div class="w-1/3">
+        <div class="flex flex-row flex-wrap gap-2">
+            <div class="w-1/4">
                 <img src="{{url('/images/principal_img.webp')}}" class="w-96 my-2" alt="" >
             </div>
             {{-- <p class="text-justify font-extralight  mb-2 text-sm md:text-base 2xl:text-base xl:text-base  mr-6 ml-6 font-inter text-black lg:text-base  lg:text-justify lg:font-extralight lg:ml-0   md:mt-6 lg:mt-0"> --}}
