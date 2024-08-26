1ffa84e090542cddafa350b697910cc7cccf7389
midhunps <midhunps.0@gmail.com>
2024-08-26 08:50:51 +0530

    added principals name

--- resources/views/components/principal.blade.php
+++ resources/views/components/principal.blade.php
@@ -1,6 +1,6 @@
 <div>
-    <div class=" shadow p-2 my-4">
-        <h class="text-teal text-base md:text-xl ml-4 lg:ml-0 font-light">Principal's Message</h>
+    <div class=" shadow p-2 my-8">
+        <h class="text-teal text-base md:text-xl ml-4 lg:ml-0 font-medium">Principal's Message</h>
         <div class="flex flex-row flex-wrap">
             <div class="w-1/3 pr-3">
                 <img src="{{url('/images/principal_img.webp')}}" class="w-96 my-2" alt="" >
@@ -11,6 +11,10 @@
                 I am very proud to be the principal of this wonderful school. We welcome all aspiring young minds and caring hearts to join the Noble profession of Nursing and pursue our knowledge and skills to serve the world at large.</p>
                 <p>Our CRAFT SCHOOL OF NURSING, offers 3-year Diploma programme in General Nursing and Midwifery (GNM) recognized by KNC and INC. The focus of our institute is to develop the personality of each student and to prepare the nurses with right knowledge, kind and proper attitude. Along with our faculty and supportive staffs, we strive to provide quality nursing education to render standard nursing care to health sector.</p>
                 <p>I, as the Principal of “CRAFT SCHOOL OF NURSING” look forward to see this institute as one of the best Nursing Institution.”</p>
+                <p class="text-right leading-6">
+                    Beena T. John<br>
+                    Principal
+                </p>
             </div>
         </div>
     </div>
