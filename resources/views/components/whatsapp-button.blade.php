6674b812a0eb3bf5e4ef35e7bfef0261cf0ee86e
midhunps <midhunps.0@gmail.com>
2024-09-04 09:53:09 +0530

    click to updated whatsap number

--- resources/views/components/whatsapp-button.blade.php
+++ resources/views/components/whatsapp-button.blade.php
@@ -1,3 +1,3 @@
-<a href="https://craftivf.com" class="inline-block w-16 h-16 fixed bottom-4 right-4 z-50" target="_blank">
+<a href="https://wa.me/919249095267" class="inline-block w-16 h-16 fixed bottom-4 right-4 z-50" target="_blank">
     <img src="/images/whatsapp.png" alt="click to whatsapp" class="h-full w-full">
 </a>
